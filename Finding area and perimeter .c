07bd222ad4b427e649bc6182ee4171fa820f433b
Aviral Bhardwaj <41833582+aviralb@users.noreply.github.com>
2019-02-15 21:01:31 +0530

    Create Finding area and perimeter .c

---
+++ Finding area and perimeter .c
@@ -0,0 +1,17 @@
+//here is the program for finding area of rectangle
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main3()
+{
+	float l, b,a;
+	printf("Enter the length of your Rectangle- \n ");
+	scanf("%f", &l);
+	printf("Enter the length of your Rectangle- \n ");
+	scanf("%f", &b);
+	a = l*b;
+	printf("area of your Rectangle is--%f", a);
+
+	_getch();
+}
